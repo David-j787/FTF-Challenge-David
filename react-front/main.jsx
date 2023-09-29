0318d9cdeccdc6c4b41e50eec5723011034306cb
David Murga Pereyra <davidpatejo@gmail.com>
2023-09-29 09:09:23 -0300

    Eslint, react plugins

---
+++ react-front/main.jsx
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client'
+import { Home } from './src/home'
+
+const root = createRoot(document.getElementById('app'))
+
+root.render(<Home />)
---
+++ react-front/src/home.jsx
@@ -0,0 +1,5 @@
+export function Home () {
+  return (
+    <h1>Hola 1</h1>
+  )
+}
